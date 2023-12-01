61258f100140919434de3cbbef271ba27c088336
Vishal Bihani <vishal.bihani2@gmail.com>
2023-12-02 01:57:19 +0530

    Added tsid struct

--- src/tsid.go
+++ src/tsid.go
@@ -1,1 +1,13 @@
 package main
+
+const (
+	TSID_EPOCH  int64 = 1672531200000 // 2023-01-01T00:00:00.000Z
+	TSID_BYTES  int8  = 8
+	TSID_CHARS  int8  = 13
+	RANDOM_BITS int8  = 22
+	RANDOM_MASK int32 = 0x003fffff
+)
+
+type Tsid struct {
+	Number int64
+}
