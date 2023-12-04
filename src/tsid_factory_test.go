a7f1fc966bcdf1343f3a7b2311a7e09f2c1bf523
Vishal Bihani <vishal.bihani2@gmail.com>
2023-12-04 17:33:33 +0530

    Added unit tests for generate with mock clock

--- src/tsid_factory_test.go
+++ src/tsid_factory_test.go
@@ -1,6 +1,11 @@
 package main
 
-import "testing"
+import (
+	"testing"
+	"time"
+
+	"github.com/stretchr/testify/assert"
+)
 
 func Test_WithNode(t *testing.T) {
 
@@ -163,4 +168,104 @@ func Test_WithRandom(t *testing.T) {
 			t.FailNow()
 		}
 	})
+}
+
+func Test_Generate(t *testing.T) {
+
+	t.Run("given clock when clock drifts time should not decrease", func(t *testing.T) {
+
+		var diff int64 = 10000
+		epoch := time.Now().UnixMilli()
+
+		clock := &MockClock{
+			millis: []int64{-1, epoch, epoch + 0, epoch + 1, epoch + 2, epoch + 3 - diff, epoch + 4 - diff, epoch + 5},
+		}
+
+		intRandom := NewIntRandomWithSupplierFunc(func() (int32, error) {
+			return 0, nil
+		})
+
+		tsidFactory, err := TsidFactoryBuilder().
+			WithClock(clock).
+			WithRandom(intRandom).
+			Build()
+		if err != nil {
+			t.Error(err.Error())
+			t.FailNow()
+		}
+
+		// Generate and record the time component
+		tsid1, _ := tsidFactory.Generate()
+		ms1 := tsid1.GetUnixMillis() // time
+
+		tsid2, _ := tsidFactory.Generate()
+		ms2 := tsid2.GetUnixMillis() // time + 0
+
+		tsid3, _ := tsidFactory.Generate()
+		ms3 := tsid3.GetUnixMillis() // time + 1
+
+		tsid4, _ := tsidFactory.Generate()
+		ms4 := tsid4.GetUnixMillis() // time + 2
+
+		tsid5, _ := tsidFactory.Generate()
+		ms5 := tsid5.GetUnixMillis() // time + 3 - diff
+
+		tsid6, _ := tsidFactory.Generate()
+		ms6 := tsid6.GetUnixMillis() // time + 4 - diff
+
+		tsid7, _ := tsidFactory.Generate()
+		ms7 := tsid7.GetUnixMillis() // time + 5
+
+		assert.Equal(t, ms1+0, ms2) // clock repeats
+		assert.Equal(t, ms1+1, ms3) // clock advanced
+		assert.Equal(t, ms1+2, ms4) // clock advanced
+		assert.Equal(t, ms1+2, ms5) // clock drift, time component should not decrease
+		assert.Equal(t, ms1+2, ms6) // clock drift, time component should not decrease
+		assert.Equal(t, ms1+5, ms7) // clock advanced
+
+	})
+
+	t.Run("given clock when encounters leap second time should not decrease", func(t *testing.T) {
+
+		second := time.Now().Second()
+		leapSecond := second - 1
+
+		clock := &MockClock{
+			millis: []int64{-1, int64(second * 1000), int64(leapSecond * 1000)},
+		}
+
+		intRandom := NewIntRandomWithSupplierFunc(func() (int32, error) {
+			return 0, nil
+		})
+
+		tsidFactory, err := TsidFactoryBuilder().
+			WithClock(clock).
+			WithRandom(intRandom).
+			Build()
+		if err != nil {
+			t.Error(err.Error())
+			t.FailNow()
+		}
+
+		// Generate and record the time component
+		tsid1, _ := tsidFactory.Generate()
+		ms1 := tsid1.GetUnixMillis() // second
+
+		tsid2, _ := tsidFactory.Generate()
+		ms2 := tsid2.GetUnixMillis() // leap second
+
+		assert.Equal(t, ms1, ms2) // time component should not decrease due to leap second
+	})
+}
+
+type MockClock struct {
+	index  int
+	millis []int64
+}
+
+func (c *MockClock) UnixMilli() int64 {
+	millis := c.millis[c.index]
+	c.index++
+
+	return millis
 }
