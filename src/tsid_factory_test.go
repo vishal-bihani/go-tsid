efaf65a42e22a7d0071e2aba297b00ef1b54ee02
Vishal Bihani <vishal.bihani2@gmail.com>
2023-12-04 14:24:43 +0530

    Added unit tests for node bits

--- src/tsid_factory_test.go
+++ src/tsid_factory_test.go
@@ -63,31 +63,65 @@ func Test_WithNode(t *testing.T) {
 	})
 }
 
-// func Test(t *testing.T) {
+func Test_WithNodeBits(t *testing.T) {
 
-// 	randomSupplier := NewMathRandomSupplier()
-// 	random := NewIntRandom(randomSupplier)
+	t.Run("given node bits should use correct node bits in tsid", func(t *testing.T) {
+		// possible node bits are from [0, 20]. testing all
+		for i := 0; i <= 20; i++ {
 
-// 	tsidFactory, _ := TsidFactoryBuilder().
-// 		WithNode(1).
-// 		WithRandom(random).
-// 		WithTime(time.Now()).
-// 		WithNodeBits(10).
-// 		WithCustomEpoch(TSID_EPOCH).
-// 		Build()
+			nodeBits := int32(i)
+			shift := RANDOM_BITS - nodeBits
+			mask := (1 << nodeBits) - 1
 
-// 	value, _ := tsidFactory.Generate()
+			node := int32(500 & mask)
+			tsidFactory, err := TsidFactoryBuilder().
+				WithNode(node).
+				WithNodeBits(nodeBits).
+				NewInstance()
+			if err != nil {
+				t.Error(err.Error())
+				t.FailNow()
+			}
 
-// 	t.Log(value.ToLong())
-// 	t.Log(value.ToString())
-// 	t.Log(value.ToBytes())
+			tsid, err := tsidFactory.Generate()
+			if err != nil {
+				t.Error(err.Error())
+				t.FailNow()
+			}
 
-// 	// 121706220661772722 -> 03C3356RR04DJ -> [1 176 99 41 177 128 17 178]
-// 	tsid := FromString("03C3356RR04DJ")
-// 	t.Log(tsid.ToLong())
+			actualNode := int32(uint32(tsid.GetRandom())>>shift) & int32(mask)
+			if actualNode != node {
+				t.FailNow()
+			}
+		}
+	})
 
-// 	bytes := []byte{1, 176, 99, 41, 177, 128, 17, 178}
-// 	tsid = FromBytes(bytes)
-// 	t.Log(tsid.ToLong())
+	t.Run("should use default node bits in tsid when not provided", func(t *testing.T) {
+		defaultNodeBits := 0
+		for i := 0; i <= 20; i++ {
 
-// }
+			shift := RANDOM_BITS - int32(defaultNodeBits)
+			mask := (1 << defaultNodeBits) - 1
+
+			node := int32(500 & mask)
+			tsidFactory, err := TsidFactoryBuilder().
+				WithNode(node).
+				NewInstance()
+			if err != nil {
+				t.Error(err.Error())
+				t.FailNow()
+			}
+
+			tsid, err := tsidFactory.Generate()
+			if err != nil {
+				t.Error(err.Error())
+				t.FailNow()
+			}
+
+			actualNode := int32(uint32(tsid.GetRandom())>>shift) & int32(mask)
+			if actualNode != 0 {
+				t.FailNow()
+			}
+		}
+	})
+}
